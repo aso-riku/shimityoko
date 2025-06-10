b55c3713e1982aa0fd720d25651bd76e0d2e0156
shimityoko09 <2401333@s.asojuku.ac.jp>
2025-06-10 10:50:18 +0900

    check.phpへ接続

--- login.php
+++ login.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <h1>ログイン</h1>
-    <form action="">
+    <form action="check.php" method="post" name="action">
         ユーザ名：<input type="text" name="user_name"><br>
         パスワード：<input type="text" name="password"><br>
-        <button name="">ログイン</button><br><br>
+        <button name="action" value="login">ログイン</button><br><br>
     </form>
     <link a href="">
 </body>
