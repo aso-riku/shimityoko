51f94dab8d22cf87aabdbd98a661ca26c743ebda
aso-riku <2401334@s.asojuku.ac.jp>
2025-06-11 12:10:55 +0900

    a

--- check.php
+++ check.php
@@ -36,10 +36,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $stmt=$pdo->prepare($sql);
         $stmt->execute([$username, $password]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $id = $pdo->lastInsertId();
 
         if(!empty($result)){
-            $_SESSION['user_id'] = $id;
+            $_SESSION['user_id'] = $result['id'];
             $_SESSION['user_name'] = $result['username'];
             header('Location: index.php');
             exit;
