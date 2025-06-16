2112bb6e95b498481e57164ee950f527ad6c26e1
HARUTOO-aso <2401331@s.asojuku.ac.jp>
2025-06-16 11:01:28 +0900

    削除機能を追加（DB削除）

---
+++ delete_task.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once "connectDB.php";
+$pdo = connectDB_local();
+
+
+$task_id = $_GET['id'] ?? '';
+$user_id = $_SESSION['user_id'] ?? '';
+
+if (!$task_id || !$user_id) {
+    exit('不正なリクエストです');
+}
+
+$stmt = $pdo->prepare("DELETE FROM todos WHERE id = ? AND user_id = ?");
+$stmt->execute([$task_id, $user_id]);
+
+
+header("Location: index.php");
+exit;
